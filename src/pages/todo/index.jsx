ae0da3628cf8023c11329a5c0313fa5a5b3958d5
garrett <njvv2599@126.com>
2020-04-26 20:12:37 +0800

    添加空白页面

---
+++ src/pages/todo/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+class TodoPage extends Component {
+  componentWillMount() {}
+
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <div>空白面</div>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default TodoPage;
